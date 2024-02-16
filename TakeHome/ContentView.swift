627597ce19a1927218ce66bcb4109521c1c3d080
Mert Durkaya <mertdurkaya@gmail.com>
2024-02-17 01:30:44 +0300

    Initial Commit

---
+++ TakeHome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TakeHome
+//
+//  Created by Mert Durkaya on 17/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TakeHome/TakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeApp.swift
+//  TakeHome
+//
+//  Created by Mert Durkaya on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
